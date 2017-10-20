cc765028377290049ff904da2b72e79f1bb80a9f
KTJ <kshtjkmr10@gmail.comgit config --global user.name KTJq>
2017-10-21 00:49:42 +0530

    To each headline add link to the full news article.Add number before headline.

--- top-news_crawler.py
+++ top-news_crawler.py
@@ -7,17 +7,22 @@ print(" "*20,"TOP STORIES - NDTV")
 print("")
 
 print('*'*80)
+count=0
 
 for div in soup.findAll('div', {'class':'new_storylising_contentwrap'}):
+    count+=1
     for url in div.findAll('a'):
-        print(url.text.strip())
+        print(" ",count,"."," ",url.text.strip())
         print("")
         break
         
     for content in div.findAll('div', class_='nstory_intro'):
         print("     ",content.text.strip())
         print("")
-        
+
+    for url in div.findAll('a'):
+        print("     Full News at: ",url.get('href'))
+        break
         
     print('*'*80)
     print("")
